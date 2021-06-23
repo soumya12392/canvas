be2bdbf000918018960dc0c7a29d7705aee9a281
soumya12392 <83289414+soumya12392@users.noreply.github.com>
2021-06-23 18:50:52 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,33 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var color = "red";
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(500, 500, 50, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e) {
+    color = document.getElementById("color").value;
+    console.log(color);
+
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+    console.log("X = " + mouse_x + " Y = " + mouse_y);
+    circle(mouse_x, mouse_y);
+}
+
+function circle(mouse_x, mouse_y) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.arc(mouse_x, mouse_y, 50, 0, 2*Math.PI);
+    ctx.stroke();
+}
+
+function clearArea() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
