6720c04cbdec879606d2f6d018d18bfca319ad28
soumya12392 <83289414+soumya12392@users.noreply.github.com>
2021-06-24 10:17:26 +0530

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -13,18 +13,22 @@ canvas.addEventListener("mousedown", my_mousedown);
 function my_mousedown(e) {
     color = document.getElementById("color").value;
     console.log(color);
+    line_width = document.getElementById("line_width").value;
+    console.log(line_width);
+    radius = document.getElementById("radius").value;
+    console.log(radius);
 
     mouse_x = e.clientX - canvas.offsetLeft;
     mouse_y = e.clientY - canvas.offsetTop;
     console.log("X = " + mouse_x + " Y = " + mouse_y);
-    circle(mouse_x, mouse_y);
+    circle(mouse_x, mouse_y, color, line_width, radius);
 }
 
-function circle(mouse_x, mouse_y) {
+function circle(mouse_x, mouse_y, color, line_width, radius) {
     ctx.beginPath();
     ctx.strokeStyle = color;
-    ctx.lineWidth = 2;
-    ctx.arc(mouse_x, mouse_y, 50, 0, 2*Math.PI);
+    ctx.lineWidth = line_width;
+    ctx.arc(mouse_x, mouse_y, radius, 0, 2*Math.PI);
     ctx.stroke();
 }
 
